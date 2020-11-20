c6f15eb7741a020a761dd52b1f0bf3da5203a246
Hanra1pj <70415666+Hanra1pj@users.noreply.github.com>
2020-11-20 19:01:47 -0400

    added comments

--- NotYourAverageRPG/Assets/Scripts/UIShop.cs
+++ NotYourAverageRPG/Assets/Scripts/UIShop.cs
@@ -1,15 +1,28 @@
-﻿using System.Collections;
+/*
+ * Author: Patrick Hanrahan
+ * Based off of Code Monkey's "Simple shop in Unity"
+ * https://www.youtube.com/watch?v=HuXy4XX0hzg
+ */
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using TMPro;
-
+/*  (INCOMPLETE)
+ * UI shop script. Makes the shop menu + buttons,
+ * as well as making it so that the shop buttons actually do things
+ * 
+ * Unfortunately, I was not able to make the button functional
+ * 
+ */
 public class UIShop : MonoBehaviour
 {
+    // Properties of shop button in heirarchy
     private Transform container;
     private Transform shopItemTemplate;
     private IShopCustomer shopCustomer;
 
+    // Finds the properties and hides them when the game awakes
     private void Awake()
     {
         container = transform.Find("container");
@@ -17,6 +30,7 @@ public class UIShop : MonoBehaviour
         shopItemTemplate.gameObject.SetActive(false);
     }
 
+    // Creates the item buttons when the game starts, and then hides it
     private void Start()
     {
         CreateItemButton(Item.ItemType.Sword, "Sword", Item.GetCost(Item.ItemType.Sword), 0);
@@ -28,6 +42,7 @@ public class UIShop : MonoBehaviour
         Hide();
     }
 
+    // Method to create item button by getting item properties
     private void CreateItemButton(Item.ItemType itemType, string itemName, int itemCost, int positionIndex)
     {
         Transform shopItemTransform = Instantiate(shopItemTemplate, container);
@@ -39,22 +54,29 @@ public class UIShop : MonoBehaviour
         shopItemTransform.Find("itemName").GetComponent<TextMeshProUGUI>().SetText(itemName);
         shopItemTransform.Find("costText").GetComponent<TextMeshProUGUI>().SetText(itemCost.ToString());
 
-        shopItemTransform.GetComponent<Button>();
-            TryBuyItem(itemType);
-
+        // Button functionality (based off tutorial, had issues getting button to work)
+        //                                 v Button script that wasn't shown in tutorial
+        /* shopItemTransform.GetComponent<Button_UI>().ClickFunc = () =>
+         * {
+         *    TryBuyItem(itemType);
+         * }
+         */
     }
 
+    // Using the shopCustomer interface, items are bought
     private void TryBuyItem(Item.ItemType itemType)
     {
         shopCustomer.BoughtItem(itemType);
     }
 
+    // Shows the shop menu
     public void Show(IShopCustomer shopCustomer)
     {
         this.shopCustomer = shopCustomer;
         gameObject.SetActive(true);
     }
 
+    // Hides the shop menu
     public void Hide()
     {
         gameObject.SetActive(false);
