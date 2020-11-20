79c5698742444d725196066e94ad36763565a3f1
Hanra1pj <70415666+Hanra1pj@users.noreply.github.com>
2020-11-20 19:04:34 -0400

    Update ISHopCustomer.cs

--- NotYourAverageRPG/Assets/Scripts/ISHopCustomer.cs
+++ NotYourAverageRPG/Assets/Scripts/ISHopCustomer.cs
@@ -1,7 +1,16 @@
-﻿using System.Collections;
+/*
+ * Author: Patrick Hanrahan
+ * Based off of Code Monkey's "Simple shop in Unity"
+ * https://www.youtube.com/watch?v=HuXy4XX0hzg
+ */
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+/*
+ * Interface for shop customer
+ */
 
+// Method(s) for customers to use to buy items
 public interface IShopCustomer
 {
     void BoughtItem(Item.ItemType itemType);
