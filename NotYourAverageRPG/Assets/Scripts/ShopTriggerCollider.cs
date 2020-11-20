80aa00b5374e9543f8a977ba466447bb6c988065
Hanra1pj <70415666+Hanra1pj@users.noreply.github.com>
2020-11-20 18:59:48 -0400

    added comments

--- NotYourAverageRPG/Assets/Scripts/ShopTriggerCollider.cs
+++ NotYourAverageRPG/Assets/Scripts/ShopTriggerCollider.cs
@@ -1,10 +1,21 @@
-﻿using System.Collections;
+/*
+ * Author: Patrick Hanrahan
+ * Based off of Code Monkey's "Simple shop in Unity"
+ * https://www.youtube.com/watch?v=HuXy4XX0hzg
+ */
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+/*
+ * Script for the shop trigger area. when a customer enters the ares,
+ * the shop will pop up on screen
+ */
 public class ShopTriggerCollider: MonoBehaviour
 {
+    // uiShop instance for the script to use
     [SerializeField] private UIShop uiShop;
+
+    // Shows the menu when the trigger area is entered
     private void OnTriggerEnter2D(Collider2D collider)
     {
         IShopCustomer shopCustomer = collider.GetComponent<IShopCustomer>();
@@ -14,6 +25,7 @@ public class ShopTriggerCollider: MonoBehaviour
         }
     }
 
+    // Hides the menu when the trigger area is exited
     private void OnTriggerExit2D(Collider2D collider)
     {
         IShopCustomer shopCustomer = collider.GetComponent<IShopCustomer>();
